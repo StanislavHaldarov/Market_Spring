00a5674fcdc8a16502d7048d42a4994d3a3ccb07
civelin <cvetelinaivanova7@gmail.com>
2023-02-20 14:31:03 +0200

    db init updated

--- Market/src/main/java/com/market/repository/RoleRepository.java
+++ Market/src/main/java/com/market/repository/RoleRepository.java
@@ -1,10 +1,15 @@
 package com.market.repository;
 
 import com.market.entity.Role;
+import com.market.entity.RoleNameEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    @Query("SELECT r FROM Role r WHERE r.name = :name")
+    public Role findRoleByName(@Param("name")RoleNameEnum name);
 }
--- Market/src/main/java/com/market/service/RoleService.java
+++ Market/src/main/java/com/market/service/RoleService.java
@@ -3,7 +3,7 @@ package com.market.service;
 import org.springframework.stereotype.Service;
 
 
+@Service
 public interface RoleService {
-
     void initRoles();
 }
