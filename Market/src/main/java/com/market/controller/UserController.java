0a68912126be62b97ba6ed4a2f16904055582c1a
Stasko04 <stanislavhaldarov@gmail.com>
2023-03-05 18:11:03 +0200

    security and user manager

---
+++ Market/src/main/java/com/market/controller/UserController.java
@@ -0,0 +1,121 @@
+package com.market.controller;
+
+import com.market.entity.User;
+import com.market.entity.binding.UserLoginBindingModel;
+import com.market.entity.binding.UserRegisterBindingModel;
+import com.market.repository.UserRepository;
+import com.market.service.UserService;
+import com.market.service.UserServiceModel;
+import com.market.utility.exception.NotFoundException;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    UserRepository userRepository;
+
+    private final UserService userService;
+    private final ModelMapper modelMapper;
+
+    public UserController(UserService userService, ModelMapper modelMapper) {
+        this.userService = userService;
+        this.modelMapper = modelMapper;
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        if(!model.containsAttribute("userLoginBindingModel")){
+            model.addAttribute("userLoginBindingModel", new UserLoginBindingModel());
+
+        }
+        return "login";}
+    @PostMapping("/login")
+    public String loginConfirm(@Valid @ModelAttribute UserLoginBindingModel userLoginBindingModel, BindingResult bindingResult, RedirectAttributes redirectAttributes, HttpSession httpSession)
+    {
+        if(bindingResult.hasErrors()){
+            redirectAttributes.addFlashAttribute("userLoginBindingModel", userLoginBindingModel);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.userLoginBindingModel", bindingResult);
+            return "redirect:login";
+        }
+        User user = userService.findUserByUsername(userLoginBindingModel.getUsername(),userLoginBindingModel.getPassword());
+
+        if(user==null)
+        {
+            redirectAttributes.addFlashAttribute("userLoginBindingModel", userLoginBindingModel);
+
+
+            return "redirect:login";
+
+        }
+
+        httpSession.setAttribute("user", user);
+        return "redirect:/index";
+    }
+
+    @GetMapping("/register")
+    public String register(Model model, @ModelAttribute("error") String error){
+        if(!model.containsAttribute("userRegisterBindingModel")){
+            model.addAttribute("userRegisterBindingModel", new UserRegisterBindingModel());
+        }
+        model.addAttribute("error", error);
+        return "register";
+    }
+    @PostMapping("/register")
+    public ModelAndView registerConfirm(@Valid @ModelAttribute UserRegisterBindingModel userRegisterBindingModel, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model ){
+
+        if(bindingResult.hasErrors() || !userRegisterBindingModel.getPassword().equals(userRegisterBindingModel.getConfirmPassword()))
+        {
+            redirectAttributes.addFlashAttribute("userRegisterBindingModel", userRegisterBindingModel);
+            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.userRegisterBindingModel", bindingResult);
+            return new ModelAndView("redirect:register");
+        }
+
+        try {
+            UserServiceModel userServiceModel = modelMapper.map(userRegisterBindingModel,UserServiceModel.class);
+            userService.registerUser(userServiceModel);
+        }
+        catch(NotFoundException e){
+            return new ModelAndView("redirect:register").addObject("error", e.getMessage());
+        }
+        return new ModelAndView("redirect:login");
+    }
+    
+    
+
+//    @GetMapping("/register")
+//    public String showRegistrationForm(Model model) {
+//        model.addAttribute("user", new User());
+//        return "/register";
+//    }
+//    @PostMapping("/process_register")
+//    public ModelAndView processRegister(@Valid User user, BindingResult bindingResult) {
+//        if(bindingResult.hasErrors()){
+//            return new ModelAndView("/register");
+//        } else{
+//            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+//            String encodedPassword = passwordEncoder.encode(user.getPassword());
+//            user.setEnabled(true);
+//            user.setPassword(encodedPassword);
+//            Role role = new Role();
+//            role.setName(RoleNameEnum.CUSTOMER);
+//            user.setRole(role);
+//            userRepository.save(user);
+//            return new ModelAndView("redirect:/login");
+//        }
+//    }
+}
---
+++ Market/src/main/java/com/market/service/UserService.java
@@ -0,0 +1,15 @@
+package com.market.service;
+
+import com.market.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+    void registerUser(UserServiceModel userServiceModel);
+
+    User findUserByUsername(String username, String password);
+    List<User> getAllUsers();
+
+    void updateUserRole(User user);
+    void deleteUser(Long id);
+}
