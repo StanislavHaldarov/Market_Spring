137a1f1a3ebfa0249ed44f5216468c4574f3ddd8
Stasko04 <stanislavhaldarov@gmail.com>
2023-03-05 18:10:00 +0200

    security and user manager

--- Market/src/main/java/com/market/repository/RoleRepository.java
+++ Market/src/main/java/com/market/repository/RoleRepository.java
@@ -7,9 +7,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     @Query("SELECT r FROM Role r WHERE r.name = :name")
     public Role findRoleByName(@Param("name")RoleNameEnum name);
+    Optional<Role> findByName(RoleNameEnum roleNameEnum);
 }
--- Market/src/main/java/com/market/service/RoleServiceImpl.java
+++ Market/src/main/java/com/market/service/RoleServiceImpl.java
@@ -2,9 +2,12 @@ package com.market.service;
 
 import com.market.entity.Role;
 import com.market.entity.RoleNameEnum;
+import com.market.entity.User;
 import com.market.repository.RoleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
@@ -17,5 +20,31 @@ public class RoleServiceImpl implements RoleService {
     }
 
 
+    @Override
+    public void initRoles() {
+        if(roleRepository.count()==0){
+            Role customer = new Role(RoleNameEnum.CUSTOMER);
+            Role employee = new Role(RoleNameEnum.EMPLOYEE);
+            Role admin = new Role(RoleNameEnum.ADMIN);
 
+            roleRepository.save(customer);
+            roleRepository.save(employee);
+            roleRepository.save(admin);
+        }
+
+    }
+    @Override
+    public List<Role> getAllRoles() {
+        List<Role> allRoles = roleRepository.findAll();
+        if(allRoles.isEmpty())
+        {
+            return null;
+        }
+        return allRoles;
+    }
+
+    @Override
+    public Role findRole(RoleNameEnum roleNameEnum) {
+        return roleRepository.findByName(roleNameEnum).orElse(null);
+    }
 }
